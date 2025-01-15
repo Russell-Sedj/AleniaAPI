2c303a7f66d8e2fee9f4362487fb0cc4e8aeed6f
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2025-01-15 02:20:13 +0100

    Ajout de la classe Etablissement dans AleniaAPI.Models
    Ajout d'une nouvelle classe `Etablissement` dans le fichier `Etablissement.cs` avec les propriétés `Id`, `Name` et `Missions`.

---
+++ Models/Etablissement.cs
@@ -0,0 +1,9 @@
+﻿namespace AleniaAPI.Models
+{
+    public class Etablissement
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Mission> Missions { get; set; }
+    }
+}
