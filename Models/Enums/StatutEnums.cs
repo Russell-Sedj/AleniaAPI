4fac9c352e5796baa37dbf9ec0293675c9232b2c
Russell-Sedj <34935151+Russell-Sedj@users.noreply.github.com>
2025-06-24 09:30:18 +0200

    Ajout des énumérations StatutCandidature et StatutMission

---
+++ Models/Enums/StatutEnums.cs
@@ -0,0 +1,16 @@
+namespace AleniaAPI.Models.Enums
+{
+    public enum StatutCandidature
+    {
+        EnCours, // En cours
+        Acceptee, // Acceptée
+        Refusee   // Refusée
+    }
+
+    public enum StatutMission
+    {
+        Ouverte, // Ouverte
+        Pourvue, // Pourvue
+        Fermee   // Fermée
+    }
+}
